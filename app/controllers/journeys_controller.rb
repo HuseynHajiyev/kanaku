ee070c5f0829e7138d6e569e3746529c8afb1609
Will Valentine <will.valentine1603@gmail.com>
2022-06-09 13:09:49 +0100

    started journey show

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -6,26 +6,26 @@ class JourneysController < ApplicationController
   end
 
   def show
+    @markers = [{ lat: 48.847387, lng: 2.340593 }, { lat: 48.854467, lng: 2.345068 }, { lat: 48.860365, lng: 2.338580 }]
+
     @journey_venues = @journey.venues
 
-    @markers = @journey_venues.geocoded.map do |journey_venue|
-      {
-        lat: journey_venue.latitude,
-        lng: journey_venue.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { journey_venue: journey_venue })
-      }
-    end
+    # @markers = @journey_venues.geocoded.map do |journey_venue|
+    #   {
+    #     lat: journey_venue.latitude,
+    #     lng: journey_venue.longitude,
+    #     info_window: render_to_string(partial: "info_window", locals: { journey_venue: journey_venue })
+    #   }
+    # end
   end
 
   def edit; end
-
 
   def preview
     @journey = current_user.journeys.last
   end
 
   def new; end
-
 
   def update
     @journey.update(journey_params)
