46e2a2a3fe969a6db01d79b1cbe266f7e6ffd82a
Harry Stanworth <harry_stan57@hotmail.com>
2022-06-09 18:34:50 +0400

    finished cities and venue seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,24 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-@city_country = ["Britain", "America", "France", "Turkey", "Germany", "Russia", "Netherlands"]
-@city_names = ["London", "New York", "Paris", "Istanbul", "Berlin", "Saint Petersburg", "Amsterdam"]
-@city_description = ["The man who can dominate a London dinner-table can dominate the world.",
-                     "The city that never sleeps!",
-                     "To know Paris is to know a great deal.",
-                     "Istanbul is inspiring because it has its own code of architecture, literature, poetry, music.",
-                     "Berlin, the greatest cultural extravaganza that one could imagine.",
-                     "St. Petersburg is a gem of world culture and Russia's most European city.",
-                     "In Europe, I always have fun bike riding in Amsterdam."]
-@city_latitude = [51.5072, 40.7128, 48.8566, 41.0082, 52.5200, 59.9311, 52.3676]
-@city_longitude = [0.1276, 74.0060, 2.3522, 28.9784, 13.4050, 30.3609, 4.9041]
+city_country = ["Britain", "America", "France", "Turkey", "Germany", "Russia", "Netherlands"]
+city_names = ["London", "New York", "Paris", "Istanbul", "Berlin", "Saint Petersburg", "Amsterdam"]
+city_description = ["The man who can dominate a London dinner-table can dominate the world.",
+                    "The city that never sleeps!",
+                    "To know Paris is to know a great deal.",
+                    "Istanbul is inspiring because it has its own code of architecture, literature, poetry, music.",
+                    "Berlin, the greatest cultural extravaganza that one could imagine.",
+                    "St. Petersburg is a gem of world culture and Russia's most European city.",
+                    "In Europe, I always have fun bike riding in Amsterdam."]
+city_latitude = [51.5072, 40.7128, 48.8566, 41.0082, 52.5200, 59.9311, 52.3676]
+city_longitude = [0.1276, 74.0060, 2.3522, 28.9784, 13.4050, 30.3609, 4.9041]
 
-@city_names.each_with_index do |city, index|
+city_names.each_with_index do |city, index|
   city = City.new(name: city)
   city.country = city_country[index]
   city.description = city_description[index]
   city.latitude = city_latitude[index]
-  city.longitude = @city_longitude[index]
+  city.longitude = city_longitude[index]
   city.save
 end
 
