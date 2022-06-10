d1e3b49b7fa864c63c8e2024e0e346436fc98707
Huseyn Hajiyev <hhajiyev@hotmail.com>
2022-06-10 14:50:05 +0300

    did the entire thing

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   get "/journeys/:id/preview", to: "journeys#preview", as: :preview
   get '/profile', to: 'profiles#show'
   root to: 'pages#home'
-  resources :journeys do
+  resources :journeys
+  resources :venues do
     resources :journey_venues, only: %i[create]
   end
-  resources :journey_venues, only: %i[create delete]
-  resources :venues
+  resources :journey_venues, only: %i[destroy]
   resources :cities, only: %i[index show new create]
 
-  post "/cities/:id", to: "journeys#add_venue", as: :add_venue
-  delete "/cities/:id", to: "journeys#remove_venue", as: :remove_venue
+  # post "/cities/:id", to: "journeys#add_venue", as: :add_venue
+  # delete "/cities/:id", to: "journeys#remove_venue", as: :remove_venue
 end
